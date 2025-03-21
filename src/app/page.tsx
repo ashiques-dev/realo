7a00bb976d805f1fd633851bf86cedf5b8613a25
ashiques-dev <ashiques.dev@gmail.com>
2025-03-21 23:24:06 +0530

    logo-ticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero";
+import LogoTicker from "@/components/logo-ticker";
 import Navbar from "@/components/navbar";
 import React from "react";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <Navbar />
       <Hero />
+      <LogoTicker />
     </>
   );
 };
